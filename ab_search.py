09bf0e540a1faf2e7fc9dbf2faf411dcafb6d684
Harlock Official <harlockofficial.github@gmail.com>
2023-10-19 23:30:11 +0200

    [edit] working alfa beta search

--- ab_search.py
+++ ab_search.py
@@ -1,5 +1,6 @@
+from game.breakthrough import Breakthrough
+from misc.utils import Infinity, Loss, MaxDepth, NoMove
 import misc.pv as pv
-from misc.utils import MaxDepth, Infinity, NoMove
 
 
 class Search:
@@ -11,13 +12,89 @@ class Search:
         self._params = params
         self._pv = pv.PV(MaxDepth)
 
-    def best_move(self, game):
+    def best_move(self, game: Breakthrough):
+        """ fail-soft alpha-beta search """
+        def mini_max(_game: Breakthrough, ply, depth, pv, alpha, beta):
+            assert (ply >= 0)
+            nonlocal nodes, do_abort
+            nodes += 1
+            if _game.is_terminal():
+                return Loss + ply
+            elif depth <= 0:
+                return self._evaluator(_game)
+            elif self._abort_checker.do_abort(0, nodes):
+                do_abort = True
+                return 0
+
+            self._pv.set_none(ply)
+            moves = self._generate_ordered_moves(ply, _game, pv[ply] if ply < len(pv) else NoMove)
+            for move in moves:
+                _game.make(move)
+                value = -mini_max(_game, ply + 1, depth - 1, pv, -beta, -alpha)
+                _game.retract(move)
+                if value > alpha:
+                    alpha = value
+                    self._pv.set(ply, move)
+                if alpha >= beta:
+                    return alpha
+                if do_abort:
+                    break
+            return alpha
+            """
+            if ply % 2 == 0:
+                # Plays White (MAX)
+                for move in moves:
+                    _game.make(move)
+                    value = mini_max(_game, ply + 1, depth - 1, pv, alpha, beta)
+                    _game.retract(move)
+                    if value > alpha:
+                        alpha = value
+                        self._pv.set(ply, move)
+                    if beta <= alpha:
+                        return alpha
+                    if do_abort:
+                        break
+                return alpha
+            else:
+                # Plays Black (MIN)
+                for move in moves:
+                    _game.make(move)
+                    value = mini_max(_game, ply + 1, depth - 1, pv, alpha, beta)
+                    _game.retract(move)
+                    if value < beta:
+                        beta = value
+                        self._pv.set(ply, move)
+                    if beta <= alpha:
+                        return beta
+                    if do_abort:
+                        break
+                return beta
+            """
+        self._abort_checker.reset()
+        nodes, do_abort = 0, False
+        pv_list = []
+        for d in range(1, MaxDepth + 1):
+            pv_list = self._pv.get_pv()
+            v = mini_max(game, 0, d, pv_list, -Infinity, Infinity)
+            if self._params.get('verbose', 0) > 0:
+                print(d, v, ' ', end='')
+                for m in self._pv.get_pv():
+                    print(game.get_board().move_to_str(m), end=' ')
+                print()
+            if do_abort or self._abort_checker.do_abort(d + 1, nodes):
+                break
+        best_move = self._pv.get(0) if self._pv.get(0) != NoMove else pv_list[0]
+        return best_move
+
+        """
         def alpha_beta(game, ply, depth, pv, alpha, beta):
-            """ fail-soft alpha-beta search """
+            # fail-soft alpha-beta search
             assert(ply >= 0)
             nonlocal nodes, do_abort
             nodes += 1
-            if game.is_terminal() or depth <= 0:
+            if game.is_terminal():
+                return Loss + ply
+            elif depth <= 0:
                 return self._evaluator(game)
             elif self._abort_checker.do_abort(0, nodes):
                 do_abort = True
@@ -56,3 +133,4 @@ class Search:
                 break
         best_move = self._pv.get(0) if self._pv.get(0) != NoMove else pv_list[0]
         return best_move
+        """
